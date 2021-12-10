d783777abde4d3435c5a86582c39cd125af638eb
v-aguiar <v.aguiar.dev@gmail.com>
2021-12-10 03:27:12 -0300

    Finished testing, all seems to work fine. Third node challenge, check :tada:

--- README.md
+++ README.md
@@ -6,14 +6,14 @@
 
 - [x] Should be able to create a new repository
 - [x] Should be able to list the projects
-- [] Should be able to update repository
-- [] Should be able to delete the repository
-- [] Should be able to give a like to the repository
-- [] It must be possible to CRUD tasks
+- [x] Should be able to update repository
+- [x] Should be able to delete the repository
+- [x] Should be able to give a like to the repository
+- [x] It must be possible to CRUD tasks
 
 ### Bussiness Logic
 
-- [] Should not be able to update a non existing repository
-- [] Should not be able to update repository likes manually
-- [] Should not be able to delete a non existing repository
-- [] Should not be able to give a like to a non existing repository
+- [x] Should not be able to update a non existing repository
+- [x] Should not be able to update repository likes manually
+- [x] Should not be able to delete a non existing repository
+- [x] Should not be able to give a like to a non existing repository
--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ function validateId(request, response, next) {
 
   const isUuid = validate( id );
 
-  if ( !isUuid ) { return response.status(400).json({ error: 'Invalid id!' }) }
+  if ( !isUuid ) { return response.status(400).json({ error: 'Invalid id!' }) };
 
   const repository = repositories.find( repository => repository.id === id);
 
-  if ( !repository ) { response.status(404).json({ error: 'Repository not found!' }) }
+  if ( !repository ) { response.status(404).json({ error: 'Repository not found!' }) };
 
   request.repository = repository;
 
@@ -32,10 +32,10 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
-  const techsArray = techs.split(`,\ `)
+  const techsArray = techs.split(`,\ `);
 
   techsArray.forEach( tech => {
-    tech = tech.trim()
+    tech = tech.trim();
   });
 
   const repository = {
@@ -52,48 +52,40 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", validateId, (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
   const { repository } = request;
 
-  const techsArray = techs.split(`,\ `)
+  if ( url ) { repository.url = url }
+  if ( title ) { repository.title = title }
+  if ( techs ) { 
 
-  techsArray.forEach( tech => {
-    tech = tech.trim()
-  });
+    const techsArray = techs.split(`,\ `);
 
-  repository.title = title;
-  repository.url = url;
-  repository.techs = techsArray;
+    techsArray.forEach( tech => {
+      tech = tech.trim();
+    });
 
-  return response.status(200).json(repository);
+    repository.techs = techsArray }
+
+  return response.status(201).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { repository } = request;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repoIndex = repositories.indexOf( repository )
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice( repoIndex, 1 );
 
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { repository } = request;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repository.likes++;
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
